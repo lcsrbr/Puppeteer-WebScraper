3a989a1f7c9169a364e97966623c84798fc0bb8f
Lucas Moura <93lucasribeiro@gmail.com>
2023-01-20 11:45:19 -0300

    refatorando o retorno do código, adicionando readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# Boas-vindas ao repositório do projeto Puppeteer-WebScraper!
+
+Projeto realizado utilizando principalmente: Node.Js, Puppeteer e Express.
+
+O projeto consiste em buscar as informações a partir do _[site WebScraper](https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops)_, buscar apenas os notebooks Lenovo, filtrar do mais barato para o mais caro, e retornar uma API para consulta com todas as informações disponíveis dos produtos.
+
+Para acessar o projeto, atente-se a cada passo descrito a seguir, e se tiver qualquer dúvida, me envie uma _[mensagem](https://portfolio-bay-omega-17.vercel.app/#contact)_ :)
+
+# Orientações
+  <summary><strong>‼️ Para acessar o projeto: </strong></summary><br />
+
+  1. Clone o repositório
+
+  - Use o comando: `git clone git@github.com:lcsrbr/Puppeteer-WebScraper.git`.
+
+  2. Instale as dependências
+
+  - `npm install`.
+
+  3. Rode a aplicação localmente
+
+  - `npm run dev`.
+
+  3. Acesse a API a partir do endereço:
+
+  - _[http://localhost:3001/](http://localhost:3001/)_
+  #
+  
+  # [Outros Projetos](https://portfolio-bay-omega-17.vercel.app/#projetos)
--- src/index.js
+++ src/index.js
@@ -9,35 +9,31 @@ const dom = async () => {
 
     const links = await page.$$eval('.title', (el) => el.map((link)  => {
         if (link.title.includes('Lenovo')) return link.href
-    }
-    ).filter((i) => i))
+    }).filter((i) => i))
 
     const arr = [];
     let counter = 0
     for (const link of links) {
         await page.goto(link)
         await page.waitForSelector('.description')
-        const title = await page.$eval('.description', (el) => el.previousElementSibling.innerHTML)
-        const description = await page.$eval('.description', (el) => el.innerHTML)
-        const price = await page.$eval('.price', (el) => el.innerHTML)
-        const storage = await page.$$eval('.swatch', (el) => el.map((btn) => {
-            if (!btn.classList.value.includes('disabled')) {
-                return btn.value
-            }
-        }).filter((i) => i))
-        const reviews = await page.$eval('.ratings', (el) => el.children[0].innerText)
-        const stars = (await page.$$eval('.glyphicon', (el) => el.map((stars) => stars))).length
         const infos = {
             id: counter += 1,
-            title,
-            description,
-            price,
-            storage,
-            reviews,
-            stars
+            title: await page.$eval('.description', (el) => el.previousElementSibling.innerHTML),
+            description:  await page.$eval('.description', (el) => el.innerHTML),
+            priceUSD: (await page.$eval('.price', (el) => el.innerHTML)).substring(1),
+            storage: await page.$$eval('.swatch', (el) => el.map((btn) => {
+                if (!btn.classList.value.includes('disabled')) {
+                    return btn.value
+                }
+            }).filter((i) => i)),
+            reviews:  (await page.$eval('.ratings', (el) => el.children[0].innerText)).trim(),
+            stars:  (await page.$$eval('.glyphicon', (el) => el.map((stars) => stars))).length
         }
         arr.push(infos)
     }
+        arr.sort((a, b) => {
+            return a.priceUSD - b.priceUSD
+        })
     page.close()
 return arr
 };

701e531f3e83bbd3d985e3dab7299dd5d0013484
Lucas Moura <93lucasribeiro@gmail.com>
2023-01-20 11:48:39 -0300

    add rota por id

--- src/server.js
+++ src/server.js
@@ -13,3 +13,11 @@ app.listen(PORT, async () => {
 app.get('/',async (_req, res) => {
     return res.status(200).json(NotesLenovo)
 })
+
+app.get('/:id',async (req, res) => {
+    const {id} = req.params
+    const filter = NotesLenovo.filter((note) => note.id === Number(id))
+    return res.status(200).json(filter)
+})
+
+
